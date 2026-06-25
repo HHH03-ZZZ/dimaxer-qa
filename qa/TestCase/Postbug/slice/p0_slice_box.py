74cecd9b883d5b6587a1c2c3cd10fa4d90cbecbc
CCQ629 <2243505437@qq.com>
2026-06-25 13:52:03 +0800

    ccq commit 3

--- qa/TestCase/Postbug/slice/p0_slice_box.py
+++ qa/TestCase/Postbug/slice/p0_slice_box.py
@@ -1,16 +1,24 @@
 import dimaxer_ui as ui
 
-# https://project.feishu.cn/dimaxer-dev/issue/detail/7013303433?parentUrl=%2Fdimaxer-dev%2Fissue%2Fhomepage
+
 def slice_box():
-    ui.import_model_post("dmh1/19-0.000949999958.dmh.dat")
-    ui.ribbon_hide_all_and_select_mesh()
+    ui.select_post_ribbon_tab()
+    ui.item_click("post.top.open_files")
+    ui.item_input_value("post.dialog.open_file_input", "/data2/cfd/ccq/qa/Model/vki_59_demo/simulations/vki_59_demo_simulation.dmx")
+    ui.wait_ui_idle()
+    ui.item_click_apply("post.case_loader.apply")
     ui.item_click("post.ribbon.hide_all")
-    ui.item_click("post.ribbon.vis_vki59")
+    ui.wait_ui_idle()
+
+    ui.item_click("post.ribbon.mesh_select_vki59_1")
+    ui.item_click("post.ribbon.vis_vki59_1")
+
     ui.item_click("post.top.slice_panel")
     ui.combo_click("post.slice.combo_type", "Box")
     ui.item_click("post.slice.show_preview")
+    ui.item_input_value("post.slice.length", "0.05")
     ui.item_click("post.slice.apply")
+
     ui.item_click("post.ribbon.hide_all")
     ui.item_click("post.ribbon.vis_slice1")
     ui.capture_renderview(8)
-
